534ada98028f3370e8854a2dfb593d639bcdbcaa
Saran Baskaran <tutysara@gmail.com>
2026-01-05 10:12:27 +0530

    feat: include config properties and setup to read it

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,9 +1,14 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PolarProperties.class)
+//@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
